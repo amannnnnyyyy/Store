5ea9a9ed1e19856662662cb7f89488db21312924
Amanuel Moha <mohaaman922@gmail.com>
2025-04-07 11:32:21 +0300

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 const font = Urbanist({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${font.className} antialiased`}
       >
+        <Navbar/>
         {children}
         <Footer/>
       </body>
