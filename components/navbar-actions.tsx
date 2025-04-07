cae5034db38ffd804d1ab1bde94f04d18d4e29c6
Amanuel Moha <mohaaman922@gmail.com>
2025-04-07 16:35:15 +0300

    prevent hydration

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -2,8 +2,17 @@
 
 import { ShoppingBag } from "lucide-react"
 import Button from "./ui/button"
+import { useEffect, useState } from "react"
 
 function NavbarActions() {
+  const [mounted , setMounted] = useState(false)
+
+  useEffect(()=>{
+    setMounted(true)
+  }, [])
+
+  if(!mounted) return null;
+
   return (
     <div className="ml-auto flex items-center gap-x-4">
         <Button disabled={false} className="flex items-center rounded-full bg-black px-4 py-2">
@@ -11,6 +20,9 @@ function NavbarActions() {
             size={20}
             color="white"
           />
+          <span className="ml-2 text-sm font-medium text-white">
+            0
+          </span>
         </Button>
     </div>
   )
